b8f8c8a7cd636ac087ea8b1b45b732c5b01598de
hashi012 <arthurhenri2006@gmail.com>
2023-07-15 10:38:53 -0300

    Colisões pelo mapa

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
    public Transform target;
-   
+
     void Update(){
-        transform.position = 
-        new Vector3(target.transform.position.x,
-        target.transform.position.y, 
-        transform.position.z);
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
 
 }
