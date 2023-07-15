d98005f6d53a0903f578c8667d59cfd323fde619
hashi012 <arthurhenri2006@gmail.com>
2023-07-15 10:51:19 -0300

    A câmera agora é limitada ao mapa

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-   public Transform target;
+    public float smoothing;
+    public Transform target;
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
 
-    void Update(){
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+    void LateUpdate(){
+
+        if (transform.position != target.position){
+
+            Vector3 targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
     }
-
 }
